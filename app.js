2f67c8ae6a87cdfabf98b408a9949c19e2290dc5
Luke Donahue <luke@expensify.com>
2023-01-15 22:37:11 -0800

    coupla changes

---
+++ app.js
@@ -0,0 +1,166 @@
+const RickAndMortyAPI = 'https://rickandmortyapi.com/api/character/';
+let favoriteCharactersData = [];
+let characterGridData = [];
+let currentPage = 1;
+
+// ARM BUTTONS
+const searchInput = document.getElementById('filter-search');
+const favoriteBar = document.getElementById('favorite-container');
+const cardContainer = document.getElementById('card-container');
+const nextPage = document.getElementById('next-page');
+const prevPage = document.getElementById('previous-page');
+searchInput.addEventListener('search', search);
+nextPage.addEventListener('click', page);
+prevPage.addEventListener('click', page);
+
+load()
+
+// This function is used to boot the app. 
+// It renders the favorites and the card grid.
+async function load() {
+    initializeLocalStorage();
+    await getData(localStorage.getItem('favorites'), 'faves');
+    await showData('faves');
+    await getData(`?page=${currentPage}`, 'grid');
+    await showData('grid');
+}
+
+// This function is used to change the page. 
+// It gets new char content and renders it in the grid.
+async function page(event) {
+    console.log(currentPage);
+    targetID = event.target.id;
+    currentPage = targetID === 'next-page' ? currentPage + 1 : currentPage - 1;
+    await getData(`?page=${currentPage}`, 'grid');
+    await showData('grid');
+}
+
+// This function is used to search for characters of a given name.
+// It gets char info and renders it to the grid. 
+async function search(event) {
+    const searchTerm = event.target.value.toUpperCase();
+    await getData(`?name=${searchTerm}`, 'grid');
+    await showData('grid');
+}
+
+// Called on heart click.
+async function favorite(event) {
+    if (!event.target.classList.contains('favoriteButton')) {
+        return;
+    }
+    
+    toggleHeartDisplay(event);
+
+    const favorites = localStorage.getItem('favorites');
+    const favoriteArray = favorites.split(',');
+    if (favoriteArray.includes(event.currentTarget.id)) {
+        favoriteArray.splice(favoriteArray.indexOf(event.currentTarget.id));
+    } else {
+        favoriteArray.push(event.currentTarget.id);
+    }
+    if (favoriteArray.length === 0) {
+        debugger;
+    }
+    localStorage.setItem('favorites', favoriteArray);
+    await getData(localStorage.getItem('favorites'));
+    await showData('faves');
+}
+
+
+async function getData(queryParam, destination) {
+    if (queryParam === '') {
+        return;
+    }
+    const data = await fetch(`${RickAndMortyAPI}${queryParam}`);
+    const parsedResponse = await data.json();
+    // check out data.info for metadata
+    if (destination === 'grid') {
+        characterGridData = parsedResponse.results;
+    } else {
+        favoriteCharactersData = Array.isArray(parsedResponse) ? parsedResponse : [parsedResponse];
+    }
+}
+
+// Looks at characterGridData and renders it in the grid.
+function showData(destination) {
+    if (destination === 'grid') {
+        cardContainer.innerHTML = '';
+    } else {
+        favoriteBar.innerHTML = '';
+    }
+    const collection = destination === 'grid' ? characterGridData : favoriteCharactersData;
+    collection.forEach((character) => {
+        if(destination === 'grid') {
+            renderCard(character);
+        } else {
+            renderFavoriteCard(character);
+        }
+    });
+}
+
+// Takes a single character's data and returns a card DOM element.
+function renderCard(characterData) {
+    let card = document.createElement('div');
+    card.id = characterData.id;
+    card.classList.add('characterCard');
+    card.addEventListener('click', favorite);
+    let cardTitle = document.createElement('div');
+    cardTitle.classList.add('characterNames');
+    let cardImg = document.createElement('img');
+    let favoriteIcon = document.createElement('img');
+    let cardText = document.createElement('ul');
+    let textSpecies = document.createElement('li');
+    let textStatus = document.createElement('li');
+    let textLocation = document.createElement('li');
+
+    favoriteIcon.src = checkFaves(card.id) ? 'img/favorite-heart.svg' : 'img/not-favorite-heart.svg'; 
+    favoriteIcon.classList.add('favoriteButton');
+    cardTitle.innerText = `${characterData.name}`;
+    cardImg.src = characterData.image;
+    textSpecies.innerText = `Species: ${characterData.species}`;
+    textStatus.innerText = `Status: ${characterData.status}`;
+    textLocation.innerText = `Location: ${characterData.location.name}`;
+    card.appendChild(cardImg);
+    card.appendChild(cardTitle);
+    card.appendChild(favoriteIcon);
+    card.appendChild(cardText);
+    cardText.appendChild(textSpecies);
+    cardText.appendChild(textStatus);
+    cardText.appendChild(textLocation);
+    cardContainer.appendChild(card);
+}
+
+function renderFavoriteCard(characterData) {
+    let favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favoriteCard');
+    favoriteCard.id = `favorite-card-${characterData.id}`;
+    let favoriteCardImg = document.createElement('img');
+    favoriteCardImg.src = characterData.image;
+    let favoriteCardName = document.createElement('div');
+    favoriteCardName.innerText = characterData.name;
+    let favoritePin = document.createElement('img');
+    favoritePin.src = 'img/thumb-tack.svg';
+    favoriteCard.appendChild(favoriteCardImg);
+    favoriteCard.appendChild(favoriteCardName);
+    favoriteCard.appendChild(favoritePin);
+    favoriteBar.appendChild(favoriteCard);
+}
+
+function toggleHeartDisplay(event) {
+    event.target.classList.toggle('favorited');
+    event.target.src = event.target.classList.contains('favorited') 
+        ? 'img/favorite-heart.svg' 
+        : 'img/not-favorite-heart.svg';
+}
+
+function checkFaves(id) {
+    const favorites = localStorage.getItem('favorites');
+    return Boolean(favorites.split(',').find(elem => elem == id));
+}
+
+function initializeLocalStorage() {
+    const faves = localStorage.getItem('favorites');
+    if (faves === null) {
+        localStorage.setItem('favorites', '');
+    }
+}
